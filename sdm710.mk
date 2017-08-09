08f71ce1db2b43d1d9bb3d4935d7caf516d5698f
David Ng <dave@codeaurora.org>
2017-08-08 17:15:24 -0700

    sdm710-common: Add common config.fs AID/fs configuration file
    Common config.fs/$TARGET_FS_CONFIG_GEN AID/fs configuration
    file for all chipset inheriting from base.mk.
    
    Since legacy device-specific android_filesystem_config.h and
    config.fs do not co-exist, temporarily include config.fs only
    if legacy file is absent.  Once all legacy, device-specific
    files have been deleted, config.fs can be made unconditional.
    
    Change-Id: Ie100d0cd9db808b82d99be7139f0709390c7f3a2

--- sdm710.mk
+++ sdm710.mk
@@ -64,6 +64,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.midi.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.midi.xml \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml
 
+# AID/fs configs
+PRODUCT_PACKAGES += \
+    fs_config_files
+
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl:32 \
