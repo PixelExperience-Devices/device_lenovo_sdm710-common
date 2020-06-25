02b355647fa02b36bc25f60ece3e4ac2081ef815
Kra1o5 <kra1o5x@gmail.com>
2020-06-25 18:57:47 +0000

    sdm710-common: Define display features via property
    Android 10 deprecates the ConfigStore HAL as we can see here:
    https://source.android.com/devices/architecture/configuration
    
    Signed-off-by: Mesquita <mickaelmendes50@gmail.com>
    Change-Id: Ia9d862190e0da544096023b28f18323c58352546
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -123,6 +123,12 @@ PRODUCT_PACKAGES += \
     memtrack.sdm710 \
     vendor.qti.hardware.display.allocator@1.0-service
 
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    ro.surface_flinger.has_HDR_display=true \
+    ro.surface_flinger.has_wide_color_display=true \
+    ro.surface_flinger.use_color_management=true \
+    ro.surface_flinger.wcg_composition_dataspace=143261696
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl-qti \
