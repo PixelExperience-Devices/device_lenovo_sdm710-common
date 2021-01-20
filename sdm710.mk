3cacb8f7b00878d7cc330e6210a16bbdd8a9a667
Chenyang Zhong <zhongcy95@gmail.com>
2021-01-20 00:43:13 -0500

    sdm710-common: clean up display stack
    libdisplayconfig was replaced by libdisplayconfig.qti on R. While
    we are at it, build vendor.disiplay.config@1.0.vendor as well so
    that QSEECOMD is happy.
    
    E QSEECOMD: : Init dlopen(libsecureui.so, RLTD_NOW) is failed.... dlopen failed: library "vendor.display.config@1.0.so" not found: needed by /vendor/lib64/libsecureui.so in namespace (default)
    
    Signed-off-by: Chenyang Zhong <zhongcy95@gmail.com>

--- sdm710.mk
+++ sdm710.mk
@@ -149,21 +149,17 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_PACKAGES += \
-    android.hardware.graphics.composer@2.3-impl \
     android.hardware.graphics.composer@2.3-service \
     android.hardware.graphics.mapper@2.0-impl-qti-display \
     android.hardware.memtrack@1.0-impl \
     android.hardware.memtrack@1.0-service \
     gralloc.sdm710 \
     hwcomposer.sdm710 \
-    libdisplayconfig \
-    libdisplayconfig.vendor \
     libdisplayconfig.qti \
-    libvulkan \
-    libdisplayconfig \
     libqdMetaData \
     libqdMetaData.system \
     libtinyxml \
+    libvulkan \
     vendor.display.config@1.0.vendor \
     vendor.display.config@2.0 \
     vendor.display.config@2.0.vendor \
