470c614f6251eb6fa09a65cd4cb30a6881292874
Bruno Martins <bgcngm@gmail.com>
2021-07-25 02:06:42 +0200

    sdm710-common: Add PocketMode app

--- device.mk
+++ device.mk
@@ -281,6 +281,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.sip.voip.xml \
     frameworks/native/data/etc/handheld_core_hardware.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/handheld_core_hardware.xml
 
+# PocketMode
+PRODUCT_PACKAGES += \
+    LenovoPocketMode
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.lenovo
