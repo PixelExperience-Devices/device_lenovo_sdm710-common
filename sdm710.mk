df5cd8ff7e3f9bac89904879352f2b1a7995ddb0
Giammarco Senatore <giammasena@gmail.com>
2021-08-16 16:31:21 +0200

    sdm710-common: Drop unneeded sensors libs
     we use prebuilt 2.0

--- sdm710.mk
+++ sdm710.mk
@@ -305,8 +305,6 @@ PRODUCT_PACKAGES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service \
     libsensorndkbridge
 
 # Soong namespaces
