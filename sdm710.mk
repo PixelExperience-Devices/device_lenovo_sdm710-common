5b5fd7d9022b800ac6d87a18f228d2fd57ad80b3
Giammarco Senatore <giammasena@gmail.com>
2021-10-20 11:14:21 +0200

    sdm710-common: Drop updatable APEX

--- sdm710.mk
+++ sdm710.mk
@@ -6,9 +6,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/lenovo/sdm710-common/sdm710-common-vendor.mk)
 
-# Enable updating of APEXes
-$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
-
 # Setup dalvik vm configs
 $(call inherit-product, frameworks/native/build/phone-xhdpi-4096-dalvik-heap.mk)
 
