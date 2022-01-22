7089a5938f34245079ab668526752e57f0144206
MadhavSaladi <baaswanthmadhav@gmail.com>
2022-01-22 13:07:59 +0100

    sdm710-common: Switch to GcamGo

--- sdm710.mk
+++ sdm710.mk
@@ -114,7 +114,7 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0.vendor \
-    Snap
+    GcamGo
 
 # Charger
 PRODUCT_PACKAGES += \
