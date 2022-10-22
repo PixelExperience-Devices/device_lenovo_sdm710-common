1c28990a9a5fd678c7d08d728a010595c40cded7
giasen <giammasena@gmail.com>
2022-10-22 12:56:31 +0200

    sdm710-common: drop GcamGo

--- sdm710.mk
+++ sdm710.mk
@@ -110,8 +110,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
-    vendor.qti.hardware.camera.device@1.0.vendor \
-    GcamGo
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 # Charger
 PRODUCT_PACKAGES += \
